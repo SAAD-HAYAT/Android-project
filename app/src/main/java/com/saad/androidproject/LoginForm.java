0eb04052a4dd2c8cfa1dc66ae85aa4b98b6f3351
SAAD-HAYAT <saadyaya5@gmail.com>
2023-05-30 04:16:23 +0500

    forms done

---
+++ app/src/main/java/com/saad/androidproject/LoginForm.java
@@ -0,0 +1,11 @@
+package com.saad.androidproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginForm extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
